5da35b908fd47e2ad0c2583284fde20727865bc0
HardikChhallani <116100549+HardikChhallani@users.noreply.github.com>
2025-02-07 00:00:27 +0530

    optimal approach

--- 1726-Tuple with Same Product.cpp
+++ 1726-Tuple with Same Product.cpp
@@ -59,3 +59,30 @@ public:
         return tuple*8;
     }
 };
+
+
+// Optimal approach O(n^2) & O(n)
+class Solution {
+public:
+    int tupleSameProduct(vector<int>& nums) {
+        unordered_map<int,int> mpp;
+        int n = nums.size();
+
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                mpp[nums[i]*nums[j]]++;
+            }
+        }
+
+        int tuples = 0;
+
+        for(auto& it : mpp){
+            if(it.second >= 2 ){
+                int count = it.second;
+                tuples += (count * (count-1))/2;
+            }
+        }
+
+        return tuples*8;
+    }
+};
