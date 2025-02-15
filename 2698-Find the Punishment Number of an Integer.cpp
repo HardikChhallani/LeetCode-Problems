2195efa90fbd67be8e711cf648169af8519a4c20
HardikChhallani <116100549+HardikChhallani@users.noreply.github.com>
2025-02-15 16:31:12 +0530

    Update 2698-Find the Punishment Number of an Integer.cpp

--- 2698-Find the Punishment Number of an Integer.cpp
+++ 2698-Find the Punishment Number of an Integer.cpp
@@ -36,3 +36,31 @@ public:
         return punish;
     }
 };
+
+// Optimal 
+// T.C - O(n*(2^log(n^2)))
+// S.C - O(log(n^2))
+class Solution {
+public:
+    bool check(int sq, int currSum, int num){
+        if(sq == 0)
+            return currSum == num;
+
+        return check(sq/10,currSum+(sq%10),num) ||
+               check(sq/100,currSum+(sq%100),num) ||
+               check(sq/1000,currSum+(sq%1000),num) ||
+               check(sq / 10000, currSum + sq % 10000, num);
+    }
+    int punishmentNumber(int n) {
+        int punish = 0;
+
+        for(int num=1; num<=n; num++){
+            int sq = num*num;
+
+            if(check(sq,0,num))
+                punish += sq;
+        }
+        
+        return punish;
+    }
+};
