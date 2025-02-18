f2939bb9367c7826bca809cdfb52251b20b7ad7d
HardikChhallani <116100549+HardikChhallani@users.noreply.github.com>
2025-02-18 11:10:37 +0530

    Create 2375-Construct Smallest Number From DI String.cpp

---
+++ 2375-Construct Smallest Number From DI String.cpp
@@ -0,0 +1,55 @@
+// Brute Force
+// T.C - O(n*n!)
+// S.C - O(n)
+class Solution {
+public:
+    void nextPermutation(string& nums){
+        int n = nums.size();
+
+        int idx = -1;
+        for(int i=n-2; i>=0; i--){
+            if(nums[i]-'0' < nums[i+1]-'0'){
+                idx = i;
+                break;
+            }
+        }
+
+        if(idx == -1){
+            reverse(nums.begin(),nums.end());
+            return ;
+        }
+
+        for(int i=n-1; i>idx; i--){
+            if(nums[i]-'0' > nums[idx]-'0'){
+                swap(nums[i],nums[idx]);
+                break;
+            }
+        }
+
+        reverse(nums.begin()+idx+1,nums.end());
+    }
+
+    bool matchesPattern(string& nums, string& pattern){
+        for(int i=0; i<pattern.size(); i++){
+            if((pattern[i] == 'I' && nums[i] > nums[i+1]) ||
+                (pattern[i] == 'D' && nums[i] < nums[i+1]))
+                    return false;
+        }
+        return true;
+    }
+    string smallestNumber(string pattern) {
+        int n = pattern.size();
+
+        string nums = "";
+        for(int i=1; i<=n+1; i++)
+            nums.push_back(i+'0');
+
+        while(!matchesPattern(nums,pattern)){
+            nextPermutation(nums);
+            cout<<nums<<endl;
+        }
+
+        return nums;
+    }
+};
+
