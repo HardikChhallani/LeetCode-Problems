017f9456abe5ffb8949e5e9a052244f8aa11dfde
HardikChhallani <116100549+HardikChhallani@users.noreply.github.com>
2025-02-18 11:34:43 +0530

    Update 2375-Construct Smallest Number From DI String.cpp

--- 2375-Construct Smallest Number From DI String.cpp
+++ 2375-Construct Smallest Number From DI String.cpp
@@ -53,3 +53,29 @@ public:
     }
 };
 
+// Optimal Code
+// T.C - O(n)
+// S.C - O(n)
+class Solution {
+public:
+    string smallestNumber(string pattern) {
+        int n = pattern.size();
+        string num = "";
+
+        int cnt = 1;
+        stack<char> st;
+
+        for(int i=0; i<=n; i++){
+            st.push(cnt+'0');
+            cnt++;
+
+            if(i==n || pattern[i] == 'I'){
+                while(!st.empty()){
+                    num += st.top();
+                    st.pop();
+                }
+            }
+        }
+        return num;
+    }
+};
