ad9b376134b3c547069ae696c8383f2e393e1c13
HardikChhallani <116100549+HardikChhallani@users.noreply.github.com>
2025-03-04 01:14:12 +0530

    Update 2161. Partition Array According to Given Pivot.cpp

--- 2161. Partition Array According to Given Pivot.cpp
+++ 2161. Partition Array According to Given Pivot.cpp
@@ -28,3 +28,47 @@ public:
         return res;
     }
 };
+
+//Approach-2 (Using pointers only)
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+public:
+    vector<int> pivotArray(vector<int>& nums, int pivot) {
+        int n = nums.size();
+
+        int lessPivot = 0;
+        int morePivot = 0;
+        int pivotCnt = 0;
+
+        for(int i=0; i<n; i++){
+            if(nums[i] == pivot) pivotCnt++;
+            else if(nums[i] < pivot) lessPivot++;
+            else morePivot++;
+        }
+
+        vector<int> res(n,0);
+        int less = 0, equal = lessPivot;
+        int more = pivotCnt+lessPivot;
+        for(int i=0; i<n; i++){
+            if(nums[i] == pivot){
+                res[equal] = nums[i];
+                equal++;
+            }
+            else if(nums[i] < pivot){
+                res[less] = nums[i];
+                less++;
+            }
+            else{
+                res[more] = nums[i];
+                more++;
+            }
+        }
+
+        return res;
+    }
+};
+
+//Approach-3 (Using pointers only)
+//T.C : O(n)
+//S.C : O(n)
