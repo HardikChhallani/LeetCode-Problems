de286124a11632fd44b3a70aee65e3ca9b74a204
HardikChhallani <116100549+HardikChhallani@users.noreply.github.com>
2025-03-04 17:51:31 +0530

    Update 1780. Check if Number is a Sum of Powers of Three.cpp

--- 1780. Check if Number is a Sum of Powers of Three.cpp
+++ 1780. Check if Number is a Sum of Powers of Three.cpp
@@ -18,3 +18,18 @@ public:
         return true;
     }
 };
+
+// T.C - O(log3(n))
+// S.C - O(1)
+class Solution {
+public:
+    bool checkPowersOfThree(int n) {
+
+        while(n > 0){
+            if(n%3 > 1) return false;
+            n /= 3;
+        }
+
+        return true;
+    }
+};
