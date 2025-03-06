a72e8af468f7ed1b82409995650fdddc56fdcfd2
HardikChhallani <116100549+HardikChhallani@users.noreply.github.com>
2025-03-06 17:54:48 +0530

    Update 2965. Find Missing and Repeated Values.cpp

--- 2965. Find Missing and Repeated Values.cpp
+++ 2965. Find Missing and Repeated Values.cpp
@@ -22,3 +22,33 @@ public:
         return {a,b};
     }
 };
+
+// T.C - O(n^2)
+// S.C - O(1)
+class Solution {
+public:
+    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
+        unordered_set<int> st;
+
+        int a=0, b=0;
+        int n = grid.size();
+        long long N = n*n;
+
+        long long sum = (N*(N+1)/2), gridSum = 0;
+        long long sqSum = (N*(N+1)*(2*N+1)/6), gridSqSum = 0;
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                gridSum += grid[i][j];
+                gridSqSum += (grid[i][j] * grid[i][j]);
+            }
+        }
+
+        int sqSumDiff = gridSqSum - sqSum;
+        int sumDiff = gridSum - sum;
+
+        a = ((sqSumDiff/sumDiff) + sumDiff)/2;
+        b = ((sqSumDiff/sumDiff) - sumDiff)/2;
+        return {a,b};
+    }
+};
