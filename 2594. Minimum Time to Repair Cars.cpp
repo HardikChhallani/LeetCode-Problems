cb5fcd4ac1daaaf92a61f857c0a5012de17f8078
HardikChhallani <116100549+HardikChhallani@users.noreply.github.com>
2025-03-17 23:51:27 +0530

    Update 2594. Minimum Time to Repair Cars.cpp

--- 2594. Minimum Time to Repair Cars.cpp
+++ 2594. Minimum Time to Repair Cars.cpp
@@ -1,1 +1,33 @@
 # Solve
+class Solution {
+public:
+    typedef long long ll;
+    long long findCars(vector<int>& ranks, ll mid){
+        ll cars = 0;
+        for(int& r : ranks){
+            cars += sqrt(mid/r);
+            if (cars >= LLONG_MAX) break;
+        }
+        return cars;
+    }
+    
+    long long repairCars(vector<int>& ranks, int cars) {
+        ll n = ranks.size();
+        ll minRank = *min_element(ranks.begin(),ranks.end());
+
+        ll left = 1, right = 1e18;
+        ll ans = LLONG_MAX;
+
+        while(left <= right){
+            ll mid = left + (right - left)/2;
+
+            if(findCars(ranks,mid) >= cars){
+                ans = mid;
+                right = mid-1;
+            }
+            else left = mid+1;
+        }
+
+        return ans;
+    }
+};
